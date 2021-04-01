31205c9848d43d0dff39edd3b7ccfdd7948c6086
Daniel Samet <danielsamet98@gmail.com>
2021-04-01 19:36:13 +0100

    fixed a bug and added an endpoint for multiplication
    Signed-off-by: Daniel Samet <danielsamet98@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, render_template, request
 
 bp = Blueprint("main", __name__)
 
@@ -7,3 +7,17 @@ bp = Blueprint("main", __name__)
 @bp.route("/index")
 def index():
     return render_template("index.html"), 200
+
+
+@bp.route("/multiply", methods=["POST"])
+def multiply():
+    matrix_1 = request.form.get("matrix_1", "", str)
+    matrix_2 = request.form.get("matrix_2", "", str)
+
+    if not (matrix_1 or matrix_2):
+        return jsonify({"msg": "At least one matrix is missing, two matrices are required for multiplication."}), 400
+
+    print("matrix 1:", matrix_1)
+    print("matrix 2:", matrix_2)
+
+    return jsonify({"result": matrix_1}), 200
